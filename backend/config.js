4bdaf2e6e01a064a20d81b7832e23c1a98282eb2
omkar gaikwad <147027748+omkarbg100@users.noreply.github.com>
2025-02-17 16:45:10 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT=5000;
+export const User='omkaruser123';
+export const Pass='Omkar123'
+export const URL="mongodb+srv://omkaruser123:Omkar123@cluster0.bkx5p.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0"
---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, URL } from "./config.js";
+import { Book } from "./model/bookmode.js";
+import router from "./routes/booksRoute.js";
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("WELCOME to MERN stack tutorials");
+});
+
+
+
+app.use('/books',router);
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("app conected to data base");
+
+    app.listen(PORT || 5000, () => {
+      console.log(`App is listing on the port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
